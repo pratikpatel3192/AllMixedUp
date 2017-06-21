9e3ec0827f7342e009258db018d97aea6579d6ba
pratik <pratik.patel68@gmail.com>
2017-06-21 14:49:40 -0400

    first

--- app/src/main/java/com/example/pratik/allmixedup/Base.java
+++ app/src/main/java/com/example/pratik/allmixedup/Base.java
@@ -3,6 +3,7 @@ package com.example.pratik.allmixedup;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -11,11 +12,15 @@ import android.widget.Toast;
  * Created by prati on 08-06-2017.
  */
 
-public class Base extends AppCompatActivity {
+public class Base extends AppCompatActivity
+{
+
+    public boolean LOGGED=false;
+    MenuInflater m;
     public boolean onCreateOptionsMenu(android.view.Menu menu)
     {
-        MenuInflater m=getMenuInflater();
-        m.inflate(R.menu.main,menu);
+        m = getMenuInflater();
+        m.inflate(R.menu.main, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -53,6 +58,30 @@ public class Base extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem logout = menu.findItem(R.id.action_logout);
+        MenuItem orderHistory = menu.findItem(R.id.action_order_history);
+        MenuItem user = menu.findItem(R.id.action_user_detail);
+        MenuItem login = menu.findItem(R.id.action_login);
+
+
+        if (LOGGED==true) { //could be button state or..?
+
+            login.setVisible(false);
+            logout.setVisible(true);
+            orderHistory.setVisible(true);
+            user.setVisible(true);
+        } else {
+            login.setVisible(true);
+            logout.setVisible(false);
+            orderHistory.setVisible(false);
+            user.setVisible(false);
+        }
+        return true; // this is important to call so that new menu is shown
     }
 
 }
--- app/src/main/java/com/example/pratik/allmixedup/LoginActivity.java
+++ app/src/main/java/com/example/pratik/allmixedup/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.pratik.allmixedup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,9 +16,9 @@ import android.widget.TextView;
 
 public class LoginActivity extends Base{
 
-    Button create_account;
+    Button Login_bth;
     TextView forgotPassword,CreateAccount;
-    boolean loggin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,12 @@ public class LoginActivity extends Base{
 
         CreateAccount= (TextView) findViewById(R.id.Create_account_login);
         forgotPassword= (TextView) findViewById(R.id.forgot_pass);
+        Login_bth= (Button) findViewById(R.id.loginbtn);
 
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loggin=true;
+
                 Intent i=new Intent(LoginActivity.this,CreateAccountActivity.class);
                 startActivity(i);
             }
@@ -39,13 +42,48 @@ public class LoginActivity extends Base{
         CreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loggin=true;
+
                 Intent i=new Intent(LoginActivity.this,CreateAccountActivity.class);
                 startActivity(i);
             }
         });
 
+        Login_bth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LOGGED=true;
+
+
+
+                Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
     }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem logout = menu.findItem(R.id.action_logout);
+        MenuItem orderHistory = menu.findItem(R.id.action_order_history);
+        MenuItem user = menu.findItem(R.id.action_user_detail);
+        MenuItem login = menu.findItem(R.id.action_login);
 
+
+        if (LOGGED==true) { //could be button state or..?
+
+            login.setVisible(false);
+            logout.setVisible(true);
+            orderHistory.setVisible(true);
+            user.setVisible(true);
+        } else {
+            login.setVisible(true);
+            logout.setVisible(false);
+            orderHistory.setVisible(false);
+            user.setVisible(false);
+        }
+        return true;
+    }
 }
